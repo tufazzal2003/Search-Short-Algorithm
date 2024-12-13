edd4237c7e2503982b93b0c2b2e0ad078ad710b3
Tufazzal Hossain <tufazzal2003@gmail.com>
2024-12-13 11:57:01 +0600

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int array[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    cout << "Unsorted array: ";
+    printArray(array, size);
+
+    bubbleSort(array, size);
+
+    cout << "Sorted array: ";
+    printArray(array, size);
+
+    return 0;
+}
+
