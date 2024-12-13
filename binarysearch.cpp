f2dac388f7687002a402bb910405f16417959236
Tufazzal Hossain <tufazzal2003@gmail.com>
2024-12-13 11:56:36 +0600

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int size, int target) {
+    int low = 0, high = size - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (array[mid] == target) {
+            return mid;
+        } else if (array[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int array[] = {10, 20, 30, 40, 50};
+    int size = sizeof(array) / sizeof(array[0]);
+    int target = 30;
+    int result = binarySearch(array, size, target);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    return 0;
+}
